f00ef490157b7a1dad05c49fabc4f1758e40d817
Laurent Mbuyu <laurentmbuyu@Laurents-MacBook-Air.local>
2022-12-11 15:57:22 +0200

    Add child process stdout and stderr

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 #!/usr/bin/env node
-const { spawn } = require('child_process');
+// const { exec } = require('child_process');
 
-const lsCommand = spawn('ls');
+// const lsCommand = spawn('ls');
 
-lsCommand.stdout.on('data', (data) => {
-    console.log(`DATA FROM CHILD PROCESS: \n ${data}`)
-});
+// lsCommand.stdout.on('data', (data) => {
+//     console.log(`DATA FROM CHILD PROCESS: \n ${data}`)
+// });
+
+const dirName = process.argv[2];
+const { exec } = require('child_process');
+const { stdout, stderr } = require('process');
+
+const cb = (error, stdout, stderr) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);    
+}
+
+exec(`mkdir -p ${dirName}`, cb);
